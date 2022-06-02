9e27b6bee58bcff2090db4e5a50a4883c4c83644
AaronGe88inTHU <37819541+AaronGe88inTHU@users.noreply.github.com>
2022-06-02 12:18:18 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftDIC
+
+A description of this package.
---
+++ Tests/SwiftDICTests/SwiftDICTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftDIC
+
+final class SwiftDICTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftDIC().text, "Hello, World!")
+    }
+}
